f8b542de3032ba9b6f00df95ef0431ec38688c87
Pallab Datta <Pallab Datta>
2016-10-06 16:37:53 +0100

    First Push

---
+++ src/main/java/com/hack/controllers/CustomerController.java
@@ -0,0 +1,29 @@
+package com.hack.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+@RequestMapping("/customer")
+public class CustomerController {
+
+	@RequestMapping(value = "/{name}", method = RequestMethod.GET)
+	public String getMovie(@PathVariable String name, ModelMap model) {
+
+		model.addAttribute("movie", name);
+		return "list";
+
+	}
+
+	@RequestMapping(value = "/hello", method = RequestMethod.GET)
+	public String getDefaultMovie(ModelMap model) {
+
+		model.addAttribute("movie", "this is default movie");
+		return "Hello World";
+
+	}
+
+}
